e25f6298c2515e5dac9c248e251900ab7432fee3
David Guyon <dguyon@gmail.com>
2016-10-05 19:11:44 +0200

    Update Composer to a more recent version
    Still a very lightweight container, not handling all case with Composer.

--- composer/build.sh
+++ composer/build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env ash
 
 # system update/install
-apk --update add curl git php php-json php-phar php-openssl
+apk add --no-cache --update curl git subversion
 
 # build
 curl -sS https://getcomposer.org/installer | php -- --filename=composer --install-dir=/usr/local/bin
 
 # system clean
-apk del curl
-rm -rf x/var/cache/apk/*
+rm -rf /var/cache/apk/*
